c27ed02002de21ec86d0c3d5147f3701577d1c77
Xuân Tú <buixuantu01@gmail.com>
2024-08-04 08:50:27 +0700

    Video 28 - 0408 - React

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// () => {}
+// Công thức tạo ra Component
+// Tên component chữ cái đầu tiên phải viết hoa
+// Component = Javascript + HTML + CSS
+const MyComponent = () => {
+  return <div> Fucking world Component</div>;
+};
+
+const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,7 +24,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Fucking World</h1>
+      {/* Gọi tag MyComponent mình tự định nghĩa ở trên */}
+      <MyComponent></MyComponent>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
